09414116ed671506d7ba4dc51bf5eece0d23265a
RolandSall <roland.salloum00@outlook.com>
2023-06-03 14:52:16 +0300

    feat: docs for payment-library

--- payment-library/src/main/java/com/rolandsall/payment/library/IPayService.java
+++ payment-library/src/main/java/com/rolandsall/payment/library/IPayService.java
@@ -1,5 +1,10 @@
 package com.rolandsall.payment.library;
 
+/***
+ * Eventually this should a library that provides abstraction
+ * that can be published on maven/gradle where you can download the dependency
+ * and implement your custom pay service
+ */
 public interface IPayService {
 
     void checkout(Payment payment);
